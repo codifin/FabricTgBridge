03f9db9fd51dbeca341f9c2677d673b41f78b099
Kodi Kiselev <217178086+codifin@users.noreply.github.com>
2026-07-01 16:41:19 +0500

    23123123

--- src/main/kotlin/cuteneko/tgbridge/mixin/ServerPlayNetworkHandlerMixin.kt
+++ src/main/kotlin/cuteneko/tgbridge/mixin/ServerPlayNetworkHandlerMixin.kt
@@ -13,23 +13,22 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo
 @Mixin(ServerPlayNetworkHandler::class)
 class ServerPlayNetworkHandlerMixin {
 
-    // aliases указывает промежуточное имя поля для сборки без refMap
     @Shadow(aliases = ["field_14115", "player"])
-    lateinit var player: ServerPlayerEntity
+    @JvmField // Хак для Kotlin, чтобы поле player превратилось в обычное Java-поле
+    var player: ServerPlayerEntity? = null
 
     @Inject(
-        // method содержит и читаемое имя, и обфусцированное method_31286
-        method = ["onChatGameMessage", "method_31286"], 
+        method = ["onChatGameMessage", "method_31286"],
         at = [At(
             value = "INVOKE",
             target = "Lnet/minecraft/server/PlayerManager;broadcast(Lnet/minecraft/text/Text;Ljava/util/function/Function;Lnet/minecraft/network/MessageType;Ljava/util/UUID;)V"
         )]
     )
     private fun onPlayerChatMessage(packet: ChatMessageC2SPacket, ci: CallbackInfo) {
-        val username = this.player.name.string
+        val currentPlayer = this.player ?: return
+        val username = currentPlayer.name.string
         val message = packet.chatMessage
         
-        // Передаем сообщение в наш Котлин-мост
         Bridge.onPlayerChat(username, message)
     }
 }
